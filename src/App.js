dc2b7614f6115596684564de451fd75603fc1247
Nital Patel <patelnital36@gmail.com>
2018-07-10 12:48:20 -0400

    Add title

--- src/App.js
+++ src/App.js
@@ -6,12 +6,12 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+        <header>
+          <img src="images/home-bg.jpg" className="img" />
+          <h1 className="App-title">Register For Atmiya Youth Shibir 2018</h1>
+            <h2>Enter your email address to see if you have an account or create an account</h2>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
     );
